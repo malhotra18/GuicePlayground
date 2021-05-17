b3c2169cb7371e02735ccb8f5f4bddcd101835a1
Aman Malhotra <amamalh@amazon.com>
2021-05-17 11:18:20 +0530

    Making class Singleton

--- src/main/java/com/drawingapp/module/AppModule.java
+++ src/main/java/com/drawingapp/module/AppModule.java
@@ -5,12 +5,13 @@ import com.drawingapp.annotations.EdgeValue;
 import com.drawingapp.services.DrawShape;
 import com.drawingapp.services.DrawSquare;
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 
 public class AppModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(DrawShape.class).to(DrawSquare.class);
+        bind(DrawShape.class).to(DrawSquare.class).in(Scopes.SINGLETON);
         bind(String.class).annotatedWith(ColorValue.class).toInstance("Purple");
         bind(Integer.class).annotatedWith(EdgeValue.class).toInstance(12);
     }
